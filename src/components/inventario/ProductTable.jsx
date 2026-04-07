a17ed6384b66a13d3a7ab52cd69e42f59b294e4c
Ivan Sanchez <ivanmateosanchez@hotmail.com>
2026-04-07 16:12:32 -0300

    Continuando

--- src/components/inventario/ProductTable.jsx
+++ src/components/inventario/ProductTable.jsx
@@ -131,13 +131,7 @@ export default function ProductTable({ productos, columnasVisibles, onSelectProd
                                             {formatCurrency(precio)}
                                         </td>
                                     )}
-                                    <td className="px-2 py-1.5 text-center">
-                                        <div className="text-slate-300 group-hover:text-blue-500 transition-colors opacity-0 group-hover:opacity-100">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 mx-auto">
-                                                <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                            </svg>
-                                        </div>
-                                    </td>
+                                    <td className="px-2 py-1.5"></td>
                                 </tr>
                             );
                         })}
